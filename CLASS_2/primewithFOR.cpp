632c50a8032609414196bad79719b5b79ff0deed
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 19:28:54 +0530

    Update primewithFOR.cpp

--- CLASS_2/primewithFOR.cpp
+++ CLASS_2/primewithFOR.cpp
@@ -8,12 +8,14 @@ int main(int argc, char** argv){
     cout<<"Enter any number to check its prime or not\n";
     cin>>number;
 
-    for(divisor = 2; (divisor*divisor) <= number; divisor++){
-        if(number%divisor == 0){
-            cout<<"It is not prime";
-            return 0;
+    for(divisor = 2; (divisor*divisor) <= number; divisor++)
+       {
+            if(number%divisor == 0)
+            {
+                cout<<"It is not prime";
+                return 0;
         
-        }
+            }
         
     }
     cout<<number<<"is prime";
