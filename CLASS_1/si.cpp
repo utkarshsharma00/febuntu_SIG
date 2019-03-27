4392f4f6e9ac8ac817f964bdf294bfcedb02f340
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 19:26:38 +0530

    Add files via upload

---
+++ CLASS_1/si.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int principle =1000;
+    int rate = 10;
+    int time = 2;
+    int simple_interest = principle * rate * time / 100;
+    cout<<"Simple interest is "<<simple_interest;
+    //return 0;
+
+
+}
