4f495aa1bff7d77bd5f809e608895290f2e5bcac
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 19:30:44 +0530

    Update gcd.cpp

--- CLASS_2/gcd.cpp
+++ CLASS_2/gcd.cpp
@@ -1,12 +1,15 @@
+//greatest common divisor
 #include<iostream>
 using namespace std;
 
-int main(int argc,char** argv){
+int main(int argc,char** argv)
+{
     int number1,number2;
     cout<<"Enter 2 numbers to find their GCD"<<endl;
     cin>>number1>>number2;
 
-    while(number1 % number2 != 0){
+    while(number1 % number2 != 0)
+    {
         int remainder = number1 % number2;
         number1 = number2;
         number2 =  remainder;
