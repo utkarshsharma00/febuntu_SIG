8cee68da951536b1490700433fa97a94e586a921
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 20:06:25 +0530

    Update si.cpp

--- CLASS_1/si.cpp
+++ CLASS_1/si.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char** argv){
-    int principle =1000;
+int main(int argc, char **argv)
+{
+    int principle = 1000;
     int rate = 10;
     int time = 2;
     int simple_interest = principle * rate * time / 100;
-    cout<<"Simple interest is "<<simple_interest;
+    cout << "Simple interest is " << simple_interest;
     //return 0;
-
-
 }
