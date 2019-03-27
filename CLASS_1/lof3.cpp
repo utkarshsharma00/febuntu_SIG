ec7407ac143b43b6c095815c6240873c7cf0aae8
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 20:05:44 +0530

    Update lof3.cpp

--- CLASS_1/lof3.cpp
+++ CLASS_1/lof3.cpp
@@ -1,20 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char** argv){
-    int a=10;
-    int b=20;
-    int c=30;
+int main(int argc, char **argv)
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
 
-    if(a>=b && a>=c){
-        cout<<"a = "<<a<<"is largest";
-    }else{
-        if(b>=c){
-            cout<<"b = "<<b<<"is largest";
-        }
-        else{
-                cout<<"c = "<<c<<"is largest";
+    if (a >= b && a >= c)
+    {
+        cout << "a = " << a << "is largest";
     }
-}
-
+    else
+    {
+        if (b >= c)
+        {
+            cout << "b = " << b << "is largest";
+        }
+        else
+        {
+            cout << "c = " << c << "is largest";
+        }
+    }
 }
