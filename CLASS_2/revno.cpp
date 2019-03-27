5c5bdf1225cc53a89de75351a57ab7179db74f73
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-27 20:07:19 +0530

    Update revno.cpp

--- CLASS_2/revno.cpp
+++ CLASS_2/revno.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc,char** argv){
+int main(int argc, char **argv)
+{
     int number;
-    cout<<"Enter any number\n";
-    cin>>number;
-    
-    int remainder;
-    while(number>0){
-        remainder = number%10;
-        cout<<remainder;
-        number = number/10;
-    }
+    cout << "Enter any number\n";
+    cin >> number;
 
- 
-    
+    int remainder;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        cout << remainder;
+        number = number / 10;
+    }
 }
