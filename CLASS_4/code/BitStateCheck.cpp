5dd563a671cffe2e3d32cca4e8c4314c6a58304b
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-11 18:05:15 +0530

    Create BitStateCheck.cpp

---
+++ CLASS_4/code/BitStateCheck.cpp
@@ -0,0 +1,24 @@
+// You can also use the below logic. This logic has been put forward by Pushp of CSE B.
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int num;
+    cin >> num;
+
+    int k;
+    cin >> k;
+
+    int x = num >> (k - 1);
+
+    if (x % 2 == 0)
+    {
+        cout << "OFF";
+    }
+    else
+    {
+        cout << "ON";
+    }
+}
